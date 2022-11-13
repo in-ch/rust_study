319ea5182735d9818cf7024bcfbff9ac910b4a56
in-ch <sxin2949@naver.com>
2022-11-13 23:20:09 +0900

    feat(:sparkles:): #2.4 반복문을 이용해 여러번 추리 허용, 종료

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,18 +10,28 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
-    let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!")
+                break;
+            },
+        }
     }
 }
 
