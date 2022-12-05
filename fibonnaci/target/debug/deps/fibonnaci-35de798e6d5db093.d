daa7b34a35bf7a5e3a71e3652d8e8d4face8fef8
in-ch <sxin2949@naver.com>
2022-12-05 19:20:21 +0900

    build(fibonnaci): fibonnaci build

---
+++ fibonnaci/target/debug/deps/fibonnaci-35de798e6d5db093.d
@@ -0,0 +1,5 @@
+/Users/seong-inchi/Desktop/dashboard/rust_proj/fibonnaci/target/debug/deps/fibonnaci-35de798e6d5db093: src/main.rs
+
+/Users/seong-inchi/Desktop/dashboard/rust_proj/fibonnaci/target/debug/deps/fibonnaci-35de798e6d5db093.d: src/main.rs
+
+src/main.rs:
---
+++ fibonnaci/target/debug/fibonnaci.d
@@ -0,0 +1,1 @@
+/Users/seong-inchi/Desktop/dashboard/rust_proj/fibonnaci/target/debug/fibonnaci: /Users/seong-inchi/Desktop/dashboard/rust_proj/fibonnaci/src/main.rs
